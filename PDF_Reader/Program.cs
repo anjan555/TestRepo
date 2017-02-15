267aaca4809768a60752b9870c32efb42a62f092
a.ramesh <anjan555@gmail.com>
2017-02-15 12:46:20 +0530

    Default Files

---
+++ PDF_Reader/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+using System.Text;
+
+public class PDF
+{
+    public static void main(string[] args)
+    {
+        var PDF = new PDF();
+        PDF.ReadPdfFile("‪C:\\Users\\a.ramesh\\Desktop\\bill.pdf");
+    }
+
+    public string ReadPdfFile(string fileName)
+    {
+        StringBuilder text = new StringBuilder();
+
+        if (File.Exists(fileName))
+        {
+            PdfReader pdfReader = new PdfReader(fileName);
+
+            for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+
+                currentText =
+                    Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8,
+                        Encoding.Default.GetBytes(currentText)));
+                text.Append(currentText);
+
+                Console.WriteLine(currentText);    //fg  
+                Console.Read();
+            }
+            pdfReader.Close();
+        }
+        return text.ToString();
+    }
+}
